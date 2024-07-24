1090d93dcc0b2f02b299cdc06813ee8ace7b6602
jackrichter <jackrichter1@gmail.com>
2024-07-24 13:26:26 +0200

    Lazy Loading images.

--- script.js
+++ script.js
@@ -208,6 +208,34 @@ allSections.forEach(section => {
 });
 
 ///////////////////////////////////////
+// LAZY LOADING IMAGES
+const imgTargets = document.querySelectorAll('img[data-src]');
+// console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  // console.log(entry);
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () =>
+    entry.target.classList.remove('lazy-img')
+  );
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+///////////////////////////////////////
 
 ///////////////////////////////////////
 ///////////////////////////////////////
